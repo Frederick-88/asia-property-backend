933f62f8b9bd72ad8d1964a4a563323dcb12f3b3
Chen Frederick <frederickfd88@gmail.com>
2021-06-03 22:11:34 +0700

    Done Users MongoDB Model

---
+++ models/UsersModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const saltRounds = 8;
+
+const UsersModel = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone_number: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "public/images/default-user.png",
+    },
+    country: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    city: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      updatedAt: "updated_at",
+      createdAt: "created_at",
+    },
+  }
+);
+UsersModel.pre("save", function (next) {
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
+});
+
+module.exports = mongoose.model("UsersModel", UsersModel);
