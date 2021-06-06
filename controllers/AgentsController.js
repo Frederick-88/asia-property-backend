99f30c5e27c6b71b2216a3b685ca9bad0c3a13cd
Chen Frederick <frederickfd88@gmail.com>
2021-06-06 18:49:13 +0700

    Done AgentsController

--- controllers/AgentsController.js
+++ controllers/AgentsController.js
@@ -3,9 +3,6 @@ require("dotenv").config();
 
 module.exports = {
   createAgent: (req, res, next) => {
-    console.log(req.body.image);
-    console.log(req.file);
-
     const obj = {
       name: req.body.name,
       email: req.body.email,
@@ -22,39 +19,43 @@ module.exports = {
       .then((response) => {
         res.status(200).json({
           status: "success",
-          message: "Successfully created a new agent!",
+          message: "Successfully created a new agent.",
           results: response,
         });
       })
-      .catch((error) => res.status(500).json(error));
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json(error);
+      });
   },
 
   updateAgentById: (req, res, next) => {
-    agentId = req.params.agentId;
-    AgentsModel.findById(agentId).then((selectedAgent) => {
-      AgentsModel.findByIdAndUpdate(
-        agentId,
-        // if request body not exist replace with the existing/old one
-        {
-          image: (req.file && req.file.path) || selectedAgent.image,
-          name: req.body.name || selectedAgent.name,
-          email: req.body.email || selectedAgent.email,
-          phone_number: req.body.phone_number || selectedAgent.phone_number,
-          country: req.body.country || selectedAgent.country,
-          city: req.body.city || selectedAgent.city,
-        },
-        { new: true } // used when we use findByIdAndUpdate, to return the edited document instead of old one
-      )
-        .then((response) => {
-          console.log(response); // todo : change message with name
-          res.status(200).json({
-            status: "success",
-            message: `Successfully edited the data of ${agentId} .`,
-            results: response,
-          });
-        })
-        .catch((error) => res.status(500).json(error));
-    });
+    const agentId = req.query.id;
+    AgentsModel.findById(agentId)
+      .then((selectedAgent) => {
+        AgentsModel.findByIdAndUpdate(
+          agentId,
+          // if request body not exist replace with the existing/old one
+          {
+            image: (req.file && req.file.path) || selectedAgent.image,
+            name: req.body.name || selectedAgent.name,
+            email: req.body.email || selectedAgent.email,
+            phone_number: req.body.phone_number || selectedAgent.phone_number,
+            country: req.body.country || selectedAgent.country,
+            city: req.body.city || selectedAgent.city,
+          },
+          { new: true } // used when we use findByIdAndUpdate, to return the edited document instead of old one
+        )
+          .then((response) => {
+            res.status(200).json({
+              status: "success",
+              message: `Successfully edited the data of ${selectedAgent.name}.`,
+              results: response,
+            });
+          })
+          .catch((error) => res.status(500).json(error));
+      })
+      .catch((error) => res.status(500).json(error));
   },
 
   getAllAgents: (req, res, next) => {
@@ -62,7 +63,7 @@ module.exports = {
       .then((response) => {
         res.status(200).json({
           status: "success",
-          message: "Successfully get all agents!",
+          message: "Successfully get all agents.",
           results: response,
         });
       })
@@ -72,10 +73,9 @@ module.exports = {
   getAgentById: (req, res, next) => {
     AgentsModel.findById(req.params.agentId)
       .then((response) => {
-        console.log(response); // todo : change message with name
         res.status(200).json({
           status: "success",
-          message: `Successfully get the data of ${req.params.agentId} !`,
+          message: `Successfully get the data of ${response.name}.`,
           results: response,
         });
       })
@@ -83,12 +83,17 @@ module.exports = {
   },
 
   deleteAgentById: (req, res, next) => {
-    AgentsModel.findByIdAndRemove(req.params.agentId)
-      .then(() => {
-        res.status(200).json({
-          status: "success",
-          message: `Successfully deleted id of ${req.params.agentName} !`,
-        });
+    const agentId = req.query.id;
+    AgentsModel.findById(agentId)
+      .then((selectedAgent) => {
+        AgentsModel.findByIdAndRemove(agentId)
+          .then(() => {
+            res.status(200).json({
+              status: "success",
+              message: `Successfully deleted ${selectedAgent.name}.`,
+            });
+          })
+          .catch((error) => res.status(500).json(error));
       })
       .catch((error) => res.status(500).json(error));
   },
